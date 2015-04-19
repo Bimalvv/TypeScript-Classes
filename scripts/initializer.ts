45784376a88f110021e3be58ca48162be29045c7
Bimal <bimalvasan@gmail.com>
2015-04-19 12:08:49 -0500

    First assignment

---
+++ scripts/initializer.ts
@@ -0,0 +1,45 @@
+﻿
+//then populate drop-downs with items from the different classes
+var request = new XMLHttpRequest();
+
+//class logic
+var recipes: Recipes;
+
+
+function recipeTypeLoader() {
+    var recipeGetter: RecipeType;
+
+    //class logic
+    recipes = JSON.parse(request.responseText);
+    var rType = document.getElementById('RecipeType');
+
+    //TODO: Instantiate the class object on one line, then call the LoadRecipeTypes on a second line to fill the drop down box.
+    //make sure pass in the correct value to the function. HINT: the object you need to pass in has already been created above.
+    var recipeType = new RecipeType(rType);
+    recipeType.LoadRecipeTypes(recipes);
+}
+
+window.onload = () => {    
+    request.onload = recipeTypeLoader;
+    request.open("get", "/JSON/recipeTypes.json", true);
+    request.send();
+};
+    
+class RecipeType {
+
+    //TODO: crete a member variable to match the constructor
+    private _recipeType: HTMLElement;
+
+    //TODO: the type is missing from the constructor. Fill that in. 
+    constructor(recipeType: HTMLElement) {
+        this._recipeType = recipeType;
+    }
+
+    LoadRecipeTypes(types: Recipes) {
+        for (var i = 0; i < types.recipeType.length; i++) {
+            var opt = document.createElement('option');
+            opt.innerHTML = types.recipeType[i].name;
+            this._recipeType.appendChild(opt);
+        }
+    }
+}
